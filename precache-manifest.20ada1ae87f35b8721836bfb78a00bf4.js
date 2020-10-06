b4198d7be5b06b802026b1a3694d7dae08a918a5
Ayan Das <ayan@home-mbp.lan>
2020-10-05 21:16:25 -0400

    Updates

---
+++ precache-manifest.20ada1ae87f35b8721836bfb78a00bf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daeae3f407b727075c8050e9d50c1a02",
+    "url": "/ayans-chirpings/index.html"
+  },
+  {
+    "revision": "c31fd0f6826e428ae463",
+    "url": "/ayans-chirpings/static/css/2.7f83aeb6.chunk.css"
+  },
+  {
+    "revision": "3fb6396fe998c7ef0d58",
+    "url": "/ayans-chirpings/static/css/main.c159f0c5.chunk.css"
+  },
+  {
+    "revision": "c31fd0f6826e428ae463",
+    "url": "/ayans-chirpings/static/js/2.d4f0b32c.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/ayans-chirpings/static/js/2.d4f0b32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb6396fe998c7ef0d58",
+    "url": "/ayans-chirpings/static/js/main.a0dd1421.chunk.js"
+  },
+  {
+    "revision": "f39acea257b3fe0340db",
+    "url": "/ayans-chirpings/static/js/runtime-main.e4812fdc.js"
+  }
+]);
